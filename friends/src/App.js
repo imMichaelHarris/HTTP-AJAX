888bdd5eee485dfc81b2aaa4792c733d6acb760c
Michael Harris <mhguitar1997@gmail.com>
2019-06-13 10:46:42 -0700

    Moved get request from friendslist to app component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import './App.css';
 import {Route} from 'react-router-dom';
+import axios from 'axios';
 import FriendList from './components/FriendList'
 import Home from './components/Home'
 import FriendForm from './components/FriendForm';
 import Friend from './components/Friend';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  state = {
+    friends: []
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => this.setState({ friends: res.data }))
+      .catch(err => console.log(err));
+  }
 
-      <Route exact path="/" component={Home} />
+  render(){
+    return (
+      <div className="App">
+  
+        <Route exact path="/" component={Home} />
+  
+        <Route exact path="/friends" render={props => <FriendList {...props} friends={this.state.friends} />} />
+        <Route path="/friends/form" render={FriendForm} />
+      </div>
+    );
+  }
 
-      <Route exact path="/friends" component={FriendList} />
-      <Route path="/friends/form" component={FriendForm} />
-    </div>
-  );
 }
 
 export default App;
--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -7,17 +7,10 @@ import FriendForm from "./FriendForm";
 import axios from "axios";
 class FriendList extends React.Component {
   state = {
-    friends: [],
     showForm: false,
     formBtnText: "Click here to add a friend"
   };
 
-  componentDidMount() {
-    axios
-      .get("http://localhost:5000/friends")
-      .then(res => this.setState({ friends: res.data }))
-      .catch(err => console.log(err));
-  }
 
   addFriend = newFriend => {
     axios
@@ -43,28 +36,34 @@ class FriendList extends React.Component {
   };
 
   render() {
-    return (
-      <>
-        <header>
-          <Link to="/">Home</Link>
-        </header>
-        <Container>
-          {this.state.friends.map(friend => (
-            <Friend key={friend.id} info={friend} />
-          ))}
-          <footer>
-            <Button onClick={this.toggleForm}>{this.state.formBtnText}</Button>
-            {this.state.showForm ? (
-              <FriendForm addFriend={this.addFriend} />
-            ) : (
-              ""
-            )}
-          </footer>
-        </Container>
-        <Route path="/friends/:id" render={props => <Friend {...props} friend={}} />
+    console.log(this.props)
+    if(!this.props.friends){
+      return <h1>Loading...</h1>
+    } else {
+      return (
+        <>
+          <header>
+            <Link to="/">Home</Link>
+          </header>
+          <Container>
+            {this.props.friends.map(friend => (
+              <Friend key={friend.id} info={friend} />
+            ))}
+            <footer>
+              <Button onClick={this.toggleForm}>{this.state.formBtnText}</Button>
+              {this.state.showForm ? (
+                <FriendForm addFriend={this.addFriend} />
+              ) : (
+                ""
+              )}
+            </footer>
+          </Container>
+              <Route path="/friends/:id" render={props => <Friend {...props} />} />
+  
+        </>
+      );
+    }
 
-      </>
-    );
   }
 }
 
